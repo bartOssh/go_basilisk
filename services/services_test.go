51fbb432eab3628af703a11f662c71f8eec637c2
bartossh <lenart.consulting@gmail.com>
2021-01-18 10:49:45 +0100

    spelling x

--- services/services_test.go
+++ services/services_test.go
@@ -2,7 +2,7 @@ package services
 
 import "testing"
 
-// CollisionsNumToCheck is a number of tokens to geenerate and chack against eachother
+// CollisionsNumToCheck is a number of tokens to generate and check against each other
 const CollisionsNumToCheck = 10000
 
 // TestRandToken is a dump token generation algorithm test
@@ -21,10 +21,10 @@ func TestRandToken(t *testing.T) {
 		}
 		secondToken, err := RandToken(128)
 		if firstToken == secondToken {
-			t.Errorf("token are in collision, two tokkens generated one after another cannot be equal\n %v == %v\n", firstToken, secondToken)
+			t.Errorf("token are in collision, two tokens generated one after another cannot be equal\n %v == %v\n", firstToken, secondToken)
 		}
 		if zeroToken == firstToken || zeroToken == secondToken {
-			t.Errorf("tokens are in collision, pattern is to obvoius and generation is not random\n repeated token : %v\n", zeroToken)
+			t.Errorf("tokens are in collision, pattern is to obvious and generation is not random\n repeated token : %v\n", zeroToken)
 		}
 	}
 }
